7d05959b6577260281832e17c435719c92ddaa2a
HRT04 <e215707@ie.u-ryukyu.ac.jp>
2024-01-02 15:10:18 +0900

    commit5

--- postinstall.js
+++ postinstall.js
@@ -1,16 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
-const directoryPath = path.resolve(__dirname, "./node_modules/three.ar.js");
-// ディレクトリが存在しない場合のみ作成する
-if (!fs.existsSync(directoryPath)) {
-  fs.mkdirSync(directoryPath);
-}
 // index.d.tsを生成するコード
 const declarationContent = 'declare module "three.ar.js";\n';
 
 // index.d.tsをプロジェクトのルートに書き込む
-fs.writeFileSync(
-  path.resolve(__dirname, "./node_modeles/three.ar.js/index.d.ts"),
-  declarationContent
-);
+fs.writeFileSync(path.resolve(__dirname, "./index.d.ts"), declarationContent);
