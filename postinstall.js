784fac29a53efd8805370cfbf72e1060e31f3fd9
HRT04 <e215707@ie.u-ryukyu.ac.jp>
2024-01-02 14:59:35 +0900

    commit3

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
 
 // index.d.tsを生成するコード
 const declarationContent = 'declare module "three.ar.js";\n';
